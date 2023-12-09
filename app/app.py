c57882eec16a0d02ba7c6e70890b88cfa40505b3
andromedalactea <bry3639@gmil.com>
2023-12-08 22:03:34 -0500

    [REFACTOR] side-bar

--- app/app.py
+++ app/app.py
@@ -7,33 +7,50 @@ from datetime import date
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], use_pages=True)
 app._favicon = ("montu.ico")
 
-# Estilo para la barra lateral
+# Estilo egipcio para la barra lateral
 sidebar_style = {
-    'backgroundColor': '#f4f4f2', # Color de fondo claro
-    'padding': '10px'
+    'backgroundColor': '#cda434',  # Dorado egipcio
+    'padding': '20px 10px',  # Aumento del padding para más espacio
+    'font-family': 'Times New Roman',  # Fuente clásica
+    'color': 'black',
+    'border-right': '2px solid black'  # Borde para separar de la página principal
+}
+
+# Estilo para enlaces en la barra lateral
+link_style = {
+    'color': 'black',
+    'fontWeight': 'bold',
+    'padding': '8px',  # Espacio alrededor de los enlaces
+    'margin': '5px 0',  # Margen entre enlaces
+    'backgroundColor': 'lightgoldenrodyellow',  # Color de fondo para cada enlace
+    'border-radius': '5px',  # Bordes redondeados
+    'border': '1px solid black',  # Borde sutil
+    'text-align': 'center',  # Centrar texto
+    'text-decoration': 'none',  # Sin subrayado
+    'display': 'block',  # Hacer que los enlaces llenen el espacio
 }
 
 # Estilo para los títulos y subtítulos
 header_style = {
-    'fontSize': 50,  # Mantenemos el tamaño original
+    'fontSize': 50,
     'textAlign': 'center',
-    'backgroundColor': '#cda434',  # Dorado egipcio
-    'color': 'black',  # Texto en negro para contraste
+    'backgroundColor': '#cda434',
+    'color': 'black',
     'font-weight': 'bold'
 }
 
 subheader_style = {
-    'fontSize': 32,  # Mantenemos el tamaño original
+    'fontSize': 32,
     'textAlign': 'center',
-    'backgroundColor': '#cda434',  # Dorado egipcio
+    'backgroundColor': '#cda434',
     'color': 'black'
 }
 
-# Creación de la barra lateral
+# Creación de la barra lateral con estilo egipcio
 sidebar = html.Div(
     [
         dbc.Nav(
-            [dbc.NavLink(page['name'], href=page['path']) for page in page_registry.values()],
+            [dbc.NavLink(page['name'], href=page['path'], style=link_style) for page in page_registry.values()],
             vertical=True,
             pills=True,
             style=sidebar_style,
