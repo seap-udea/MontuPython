cb84fc6be07b3cf1fe42f98089260b3ea31c7d94
Web Developer <jorge.zuluaga@udea.edu.co>
2026-07-10 10:10:39 -0500

    Update version numbers to 0.20.0 in setup.py and montu/version.py, and add new version entry in .versions file.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         ],
-    version='0.10.0',
+    version='0.20.0',
 
     # ######################################################################
     # FILES
